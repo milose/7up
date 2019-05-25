9366feeedfbfef62adf2c12b7b8022ec54adc1e8
Milos <m@deva.co>
2019-05-25 23:37:09 +0200

    Add a link to home page when logged in.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,8 @@
                             href="{{ route('register') }}">{{ __('Register') }}</a>
                         @endif
                         @else
-                        <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
+                        <span class="text-gray-300 text-sm pr-4">
+                            <a href="{{route('home')}}">{{ Auth::user()->name }}</a></span>
 
                         <a href="{{ route('logout') }}" class="no-underline hover:underline text-gray-300 text-sm p-3"
                             onclick="event.preventDefault();
