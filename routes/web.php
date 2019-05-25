9e69ccbeb63ac473c46b9bd26f81c7ec79616530
Milos <m@deva.co>
2019-05-25 23:35:01 +0200

    Fix namespace and location of admin files.

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ Route::get('/', 'PagesController@index');
 
 Auth::routes();
 
-Route::post('/', 'FileController@store');
-Route::get('/{file}', 'FileController@show');
-
 Route::middleware(['auth'])
     ->namespace('Admin')
     ->group(function () {
         Route::get('/home', 'HomeController@index')->name('home');
     });
+
+Route::post('/', 'FileController@store');
+Route::get('/{file}', 'FileController@show');
 
 // @TODO: Schedule an hourly job to clean up old files.
